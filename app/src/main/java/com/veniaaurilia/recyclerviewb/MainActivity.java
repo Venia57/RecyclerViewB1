998bb3c2d7158ddeab9e7ce1d9ed43c7d2e45aa3
Venia57 <Veniaaurilia57@gmail.com>
2024-06-07 14:56:44 +0700

    first commit

---
+++ app/src/main/java/com/veniaaurilia/recyclerviewb/MainActivity.java
@@ -0,0 +1,66 @@
+package com.veniaaurilia.recyclerviewb;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
+import java.util.List;
+
+import cz.msebera.android.httpclient.Header;
+
+public class MainActivity extends AppCompatActivity {
+    
+    private FloatingActionButton _addButton;
+    private RecyclerView _recyclerView1;
+    private TextView _txtMahasiswaCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        _recyclerView1 = findViewById(R.id.recyclerView1);
+        _txtMahasiswaCount = findViewById(R.id.txtMahasiswaCount);
+
+        loadRecyclerView();
+    }
+
+    private void loadRecyclerView() {
+        AsyncHttpClient ahc = new AsyncHttpClient();
+        String url = "http://stmikpontianak.net/011100862/tampilMahasiswa.php";
+
+        ahc.get(url, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody)
+            {
+                Gson g = new Gson();
+                List<MahasiswaModel>mahasiswaModelList = g.fromJson( new String(responseBody), new TypeToken<List<MahasiswaModel>>(){}.getType());
+                RecyclerView.LayoutManager lm = new LinearLayoutManager( MainActivity.this);
+                _recyclerView1.setLayoutManager(lm);
+
+                MahasiswaAdapter ma = new MahasiswaAdapter(mahasiswaModelList);
+                _recyclerView1.setAdapter(ma);
+
+                String mahasiswaCount = "Total Mahasiswa :  " + ma.getItemCount();
+                _txtMahasiswaCount.setText(mahasiswaCount);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error)
+            {
+                Toast.makeText(getApplicationContext(), error.getMessage(),Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
